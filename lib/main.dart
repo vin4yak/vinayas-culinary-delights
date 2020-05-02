46637b52dc495ed78ddd0b8277658cf468220b0d
vin4yak <vinayakprabhu92@gmail.com>
2020-05-03 02:08:26 +0530

    [Vinayak] Add SliverAppBar to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:vinayas_culinary_delights/recent/recent_posts.dart';
 
 import 'category/category_page.dart';
-import 'domain/push_message.dart';
 import 'drawer/app_drawer.dart';
 
 void main() => runApp(CulinaryDelightsApp());
@@ -20,17 +18,58 @@ class CulinaryDelightsApp extends StatelessWidget {
       ),
       home: DefaultTabController(
         length: 2,
-        child: Scaffold(
-          drawer: AppDrawer(),
-          appBar: AppBar(
-            bottom: TabBar(
-              tabs: [
-                Tab(text: "Recent Posts"),
-                Tab(text: "Categories")
-              ],
-            ),
-            title: Text("Vinaya's Culinary Delights"),
-          ),
+        child: MainPageWidget(),
+      )
+    );
+  }
+}
+
+class MainPageWidget extends StatelessWidget {
+  const MainPageWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: AppDrawer(),
+      body: DefaultTabController(
+        length: 2,
+        child: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                expandedHeight: 150.0,
+                floating: true,
+                pinned: true,
+                snap: false,
+                flexibleSpace: FlexibleSpaceBar(
+                    centerTitle: true,
+                    title: Text("Vinaya's Culinary Delights",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0,
+                        )),
+                    background: Image.asset(
+                      "assets/culinary_delights_header.jpg",
+                      fit: BoxFit.cover,
+                    )),
+              ),
+              SliverPersistentHeader(
+                delegate: _SliverAppBarDelegate(
+                  TabBar(
+                    labelColor: Colors.white,
+                    unselectedLabelColor: Colors.grey,
+                    tabs: [
+                      Tab(text: "Recent Posts"),
+                      Tab(text: "Categories"),
+                    ],
+                  ),
+                ),
+                pinned: true,
+              ),
+            ];
+          },
           body: TabBarView(
             children: [
               RecentPostsPage(),
@@ -38,56 +77,31 @@ class CulinaryDelightsApp extends StatelessWidget {
             ],
           ),
         ),
-      )
+      ),
     );
   }
 }
 
-class MainPageWidget extends StatefulWidget {
-  @override
-  _MainWidgetState createState() => _MainWidgetState();
-}
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate(this._tabBar);
 
-class _MainWidgetState extends State<MainPageWidget>{
-  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
-  final List<PushMessage> pushMessages = [];
+  final TabBar _tabBar;
 
   @override
-  void initState() {
-    super.initState();
+  double get minExtent => _tabBar.preferredSize.height;
+  @override
+  double get maxExtent => _tabBar.preferredSize.height;
 
-    _firebaseMessaging.configure(
-        onMessage: (Map<String, dynamic> message) async {
-          print("onMessage: $message");
-          final notification = message['notification'];
-
-          setState(() {
-            pushMessages.add(PushMessage(
-              title: notification['title'],
-              body: notification['body'],
-            ));
-          });
-        },
-        onLaunch: (Map<String, dynamic> message) async {
-          print("onLaunch: $message");
-        },
-        onResume: (Map<String, dynamic> message) async {
-          print("onResume: $message");
-        }
-    );
-    _firebaseMessaging.requestNotificationPermissions(
-        const IosNotificationSettings(sound: true, badge: true, alert: true)
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return new Container(
+      child: _tabBar,
     );
   }
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return TabBarView(
-      children: [
-        RecentPostsPage(),
-        Categories()
-      ],
-    );
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return false;
   }
 }

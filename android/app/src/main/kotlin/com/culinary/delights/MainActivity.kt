ede9966c21582a44f7bcd65b67e885b3d3b3f187
vin4yak <vinayakprabhu92@gmail.com>
2020-05-03 11:16:18 +0530

    [Vinayak] Fix android splash screen

--- android/app/src/main/kotlin/com/culinary/delights/MainActivity.kt
+++ android/app/src/main/kotlin/com/culinary/delights/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.culinary.delights
 
+import android.os.Bundle
 import androidx.annotation.NonNull;
-import io.flutter.embedding.android.FlutterActivity
+//import io.flutter.embedding.android.FlutterActivity
+import io.flutter.app.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
+//Class for flutter embedding in AndroidManifest
+//class MainActivity: FlutterActivity() {
+//    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+//        GeneratedPluginRegistrant.registerWith(flutterEngine);
+//    }
+//}
+
 class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
     }
 }

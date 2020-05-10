9ef7319c55d9949869de6a639395b46e83d37b18
vin4yak <vinayakprabhu92@gmail.com>
2020-05-10 16:39:28 +0530

    [Vinayak] Add PhotoView to handle images in food art

--- lib/art/food_art_page.dart
+++ lib/art/food_art_page.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
 import 'package:vinayas_culinary_delights/domain/food_art.dart';
 import 'package:vinayas_culinary_delights/util/service_gateway.dart';
 
@@ -45,11 +47,60 @@ class _FoodArtPageState extends State {
         mainAxisSpacing: 2.0
       ),
       itemBuilder: (BuildContext context, int index) {
-        return Container(
-          child: Image.network(foodArt[index].imageUrl,
-          fit: BoxFit.cover,)
+        return new InkWell(
+          child: Hero(
+                  tag: foodArt[index].title,
+                  child: CachedNetworkImage(
+                    imageUrl: foodArt[index].imageUrl,
+                    fit: BoxFit.cover,
+                  )
+              ),
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (_) {
+              return FoodArtDetailScreen(
+                imageUrl: foodArt[index].imageUrl,
+                title: foodArt[index].title,
+              );
+            }));
+          },
         );
       },
+    );
+  }
+
+}
+
+class FoodArtDetailScreen extends StatelessWidget {
+  final String imageUrl;
+  final String title;
+
+  FoodArtDetailScreen(
+      {
+        Key key,
+        @required this.imageUrl,
+        @required this.title
+      }
+      ) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        child: Center(
+          child: Hero(
+            tag: title,
+            child: PhotoView(
+              imageProvider: CachedNetworkImageProvider(imageUrl),
+              initialScale: PhotoViewComputedScale.contained,
+              minScale: PhotoViewComputedScale.contained,
+              maxScale: PhotoViewComputedScale.contained * 1.5,
+            ),
+          ),
+        ),
+        onTap: () {
+          Navigator.pop(context);
+        },
+      ),
     );
   }
 
